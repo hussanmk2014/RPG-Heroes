ba5804014712dff2f246405b700e2f8c521eabde
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-02-18 13:35:14 +0100

    feat: add Main menu to the game

--- src/main/java/menus/MainMenu.java
+++ src/main/java/menus/MainMenu.java
@@ -1,4 +1,49 @@
 package menus;
+import characters.Character;
+import exceptions.InvalidWeaponException;
+import static menus.EquipItemsMenu.equipItems;
+import static menus.characterCreationMenu.characterCreation;
+
+import java.util.Scanner;
+
+
 
 public class MainMenu {
+
+
+    public static void GameMenu() throws InvalidWeaponException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("<<<< Start the Game !>>>>");
+        Character character = characterCreation();
+        boolean goOn = true;
+        do {
+            System.out.println("The main Menu:");
+            System.out.println("1.Create new character (Hero)\n2.Equip some Equipments\n3.Level up\n4.Show character State\n5.Quit");
+            int selection = scanner.nextInt();
+            switch (selection){
+                case 1:
+                    character = characterCreation();
+                    break;
+                case 2:
+                    try {
+                        character = equipItems(character);
+                    }catch (Exception e){
+                        System.out.println(e);
+                    }
+                    break;
+                case 3:
+                    character.plusOneLevel();
+                    System.out.println("You reached level " + character.getLevel() + "!\n");
+                    break;
+                case 4:
+                    System.out.println(character.displayCharacterStatistics());
+                    break;
+                case 5:
+                    goOn=false;
+                    System.out.println("The Game is cancelled");
+                    break;
+            }
+
+        }while (goOn);
+    }
 }
