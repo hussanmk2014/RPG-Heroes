fb615b8d891a9160744cdd1c1749c65b6cf864cc
hussanmk2014 <44656569+hussanmk2014@users.noreply.github.com>
2023-02-18 13:38:04 +0100

    feat: add Main menu to the main class

---
+++ src/main/java/org/example/RpgHeroesApplication.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import exceptions.InvalidWeaponException;
+import menus.MainMenu;
+public class RpgHeroesApplication {
+    public static void main(String[] args) throws InvalidWeaponException {
+        MainMenu.GameMenu();
+    }
+}
